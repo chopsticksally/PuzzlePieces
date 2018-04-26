0fdd93b373d3cb30ab4865419a351d09bde97607
Xian Zheng <xzheng2112@gmail.com>
2018-04-26 13:23:02 -0600

    user controller and userDAO created

---
+++ MVCPuzzlePieces/src/main/java/com/skilldistillery/puzzlepieces/data/UserDAOImpl.java
@@ -0,0 +1,5 @@
+package com.skilldistillery.puzzlepieces.data;
+
+public class UserDAOImpl implements UserDAO{
+
+}
--- MVCPuzzlePieces/src/main/java/com/skillydistillery/puzzlepieces/controllers/UserController.java
+++ MVCPuzzlePieces/src/main/java/com/skillydistillery/puzzlepieces/controllers/UserController.java
@@ -1,7 +1,34 @@
 package com.skillydistillery.puzzlepieces.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.skilldistillery.puzzlepieces.data.UserDAO;
 
+@Controller
 public class UserController {
+	@Autowired
+	UserDAO dao;
 
+	@RequestMapping(path = "home.do", method = RequestMethod.GET)
+	public String homePage() {
+		return "home.jsp";
+	}
+
+	@RequestMapping(path = "login.do", method = RequestMethod.GET)
+	public String loginPage() {
+		return "login.jsp";
+
+	}
+
+	@RequestMapping(path = "loggedIn.do")
+	public ModelAndView loggedIn() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("logged-in");
+		return mv;
+
+	}
 }
