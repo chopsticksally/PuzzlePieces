2d24262d9a9745b0dbbbc61fdcb84949a6bef86d
Xian Zheng <xzheng2112@gmail.com>
2018-04-27 11:36:25 -0600

    stubbed out controller tests

---
+++ MVCPuzzlePieces/src/test/java/com/skilldistillery/test/controllers/PuzzleTest.java
@@ -0,0 +1,14 @@
+package com.skilldistillery.test.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PuzzleTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
